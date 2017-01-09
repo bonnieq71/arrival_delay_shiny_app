a9a78fff04f468d1753682d59532f95acbf7d581
bonnieq71 <quachgiatue@gmail.com>
2017-01-08 23:25:51 -0500

    code here

---
+++ app.R
@@ -0,0 +1,293 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(cowplot)
+library(scales)
+library(DT)
+
+flights_2008 <- readRDS("flights_2008.RDS")
+flights_2007 <- readRDS("flights_2007.RDS")
+flights_2006 <- readRDS("flights_2006.RDS")
+flights_2005 <- readRDS("flights_2005.RDS")
+flights_2004 <- readRDS("flights_2004.RDS")
+flights_2003 <- readRDS("flights_2003.RDS")
+flights_2002 <- readRDS("flights_2002.RDS")
+flights_2001 <- readRDS("flights_2001.RDS")
+flights_2000 <- readRDS("flights_2000.RDS")
+flights_1999 <- readRDS("flights_1999.RDS")
+flights_1998 <- readRDS("flights_1998.RDS")
+flights_1997 <- readRDS("flights_1997.RDS")
+flights_1996 <- readRDS("flights_1996.RDS")
+flights_1995 <- readRDS("flights_1995.RDS")
+flights_1994 <- readRDS("flights_1994.RDS")
+flights_1993 <- readRDS("flights_1993.RDS")
+flights_1992 <- readRDS("flights_1992.RDS")
+flights_1991 <- readRDS("flights_1991.RDS")
+flights_1990 <- readRDS("flights_1990.RDS")
+flights_1989 <- readRDS("flights_1989.RDS")
+flights_1988 <- readRDS("flights_1988.RDS")
+flights_1987 <- readRDS("flights_1987.RDS")
+
+ui <- fluidPage( 
+  
+  titlePanel("Arrival Delays"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("year",
+                  "Pick a Year: ",
+                  min = 1987,
+                  max = 2008,
+                  value = 1,
+                  sep = "")
+      
+    ),
+    
+    mainPanel(
+      h4("Here is a simple app allowing us to look at arrival delays in different years and then parseing them into different months. The mean delays and a few simple statistics are in the data table below. Enjoy! Data courtesy of http://stat-computing.org/dataexpo/2009/the-data.html", align = "center"),
+      br(),
+      br(),
+      plotOutput("distPlot"),
+      br(),
+      br(),
+      plotOutput("distPlot_month"),
+      br(),
+      br(),
+      dataTableOutput("month_stats")
+      )
+  )
+) 
+server <- shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    if(input$year == 2008) {
+      flights_year <- flights_2008
+    }
+    else if (input$year == 2007) {
+      flights_year <- flights_2007
+    }
+    else if (input$year == 2006) {
+      flights_year <- flights_2006
+    }
+    else if (input$year == 2005) {
+      flights_year <- flights_2005
+    }
+    else if (input$year == 2004) {
+      flights_year <- flights_2004
+    }
+    else if (input$year == 2003) {
+      flights_year <- flights_2003
+    }
+    else if (input$year == 2002) {
+      flights_year <- flights_2002
+    }
+    else if (input$year == 2001) {
+      flights_year <- flights_2001
+    }
+    else if (input$year == 2000) {
+      flights_year <- flights_2000
+    }
+    else if (input$year == 1999) {
+      flights_year <- flights_1999
+    }
+    else if (input$year == 1998) {
+      flights_year <- flights_1998
+    }
+    else if (input$year == 1997) {
+      flights_year <- flights_1997
+    }
+    else if (input$year == 1996) {
+      flights_year <- flights_1996
+    }
+    else if (input$year == 1995) {
+      flights_year <- flights_1995
+    }
+    else if (input$year == 1994) {
+      flights_year <- flights_1994
+    }
+    else if (input$year == 1993) {
+      flights_year <- flights_1993
+    }
+    else if (input$year == 1992) {
+      flights_year <- flights_1992
+    }
+    else if (input$year == 1991) {
+      flights_year <- flights_1991
+    }
+    else if (input$year == 1990) {
+      flights_year <- flights_1990
+    }
+    else if (input$year == 1989) {
+      flights_year <- flights_1989
+    }
+    else if (input$year == 1988) {
+      flights_year <- flights_1988
+    }
+    else if (input$year == 1987) {
+      flights_year <- flights_1987
+    }
+    
+    distPlot<- ggplot(flights_year, aes(x = ArrDelay)) + geom_histogram(aes(y=..count../sum(..count..)), binwidth = 15, alpha = 0.3, color = "blue", fill = "blue") + xlim(-50, 210) + scale_y_continuous(labels=percent) + labs(x = "Arrival Delay (minutes)", y = "Percent of Flights") + title("Arrival Delay by Year")
+    
+    print(distPlot)
+    
+  })
+  
+  output$distPlot_month <- renderPlot({
+    if(input$year == 2008) {
+      flights_year <- flights_2008
+    }
+    else if (input$year == 2007) {
+      flights_year <- flights_2007
+    }
+    else if (input$year == 2006) {
+      flights_year <- flights_2006
+    }
+    else if (input$year == 2005) {
+      flights_year <- flights_2005
+    }
+    else if (input$year == 2004) {
+      flights_year <- flights_2004
+    }
+    else if (input$year == 2003) {
+      flights_year <- flights_2003
+    }
+    else if (input$year == 2002) {
+      flights_year <- flights_2002
+    }
+    else if (input$year == 2001) {
+      flights_year <- flights_2001
+    }
+    else if (input$year == 2000) {
+      flights_year <- flights_2000
+    }
+    else if (input$year == 1999) {
+      flights_year <- flights_1999
+    }
+    else if (input$year == 1998) {
+      flights_year <- flights_1998
+    }
+    else if (input$year == 1997) {
+      flights_year <- flights_1997
+    }
+    else if (input$year == 1996) {
+      flights_year <- flights_1996
+    }
+    else if (input$year == 1995) {
+      flights_year <- flights_1995
+    }
+    else if (input$year == 1994) {
+      flights_year <- flights_1994
+    }
+    else if (input$year == 1993) {
+      flights_year <- flights_1993
+    }
+    else if (input$year == 1992) {
+      flights_year <- flights_1992
+    }
+    else if (input$year == 1991) {
+      flights_year <- flights_1991
+    }
+    else if (input$year == 1990) {
+      flights_year <- flights_1990
+    }
+    else if (input$year == 1989) {
+      flights_year <- flights_1989
+    }
+    else if (input$year == 1988) {
+      flights_year <- flights_1988
+    }
+    else if (input$year == 1987) {
+      flights_year <- flights_1987
+    }
+    
+    #fit <- fitdist(flights_year$ArrDelay, "norm")
+    flights_year$Month <- factor(flights_year$Month, levels = c(1:12))
+    distPlot_month <- ggplot(flights_year, aes(x=ArrDelay)) + geom_density(aes(group = Month, color = Month, fill = Month), alpha = 0.3, binwidth = 15) + xlim(-50, 50) + scale_y_continuous(labels=percent) + labs(x = "Arrival Delay (minutes)", y = "Density") + title("Arrival Delay by Month")
+    print(distPlot_month)
+    
+  })
+  
+  
+  by_month= reactive({
+    
+    if(input$year == 2008) {
+      flights_year <- flights_2008
+    }
+    else if (input$year == 2007) {
+      flights_year <- flights_2007
+    }
+    else if (input$year == 2006) {
+      flights_year <- flights_2006
+    }
+    else if (input$year == 2005) {
+      flights_year <- flights_2005
+    }
+    else if (input$year == 2004) {
+      flights_year <- flights_2004
+    }
+    else if (input$year == 2003) {
+      flights_year <- flights_2003
+    }
+    else if (input$year == 2002) {
+      flights_year <- flights_2002
+    }
+    else if (input$year == 2001) {
+      flights_year <- flights_2001
+    }
+    else if (input$year == 2000) {
+      flights_year <- flights_2000
+    }
+    else if (input$year == 1999) {
+      flights_year <- flights_1999
+    }
+    else if (input$year == 1998) {
+      flights_year <- flights_1998
+    }
+    else if (input$year == 1997) {
+      flights_year <- flights_1997
+    }
+    else if (input$year == 1996) {
+      flights_year <- flights_1996
+    }
+    else if (input$year == 1995) {
+      flights_year <- flights_1995
+    }
+    else if (input$year == 1994) {
+      flights_year <- flights_1994
+    }
+    else if (input$year == 1993) {
+      flights_year <- flights_1993
+    }
+    else if (input$year == 1992) {
+      flights_year <- flights_1992
+    }
+    else if (input$year == 1991) {
+      flights_year <- flights_1991
+    }
+    else if (input$year == 1990) {
+      flights_year <- flights_1990
+    }
+    else if (input$year == 1989) {
+      flights_year <- flights_1989
+    }
+    else if (input$year == 1988) {
+      flights_year <- flights_1988
+    }
+    else if (input$year == 1987) {
+      flights_year <- flights_1987
+    }
+    
+  group_month <- group_by(flights_year, Month)
+  month_summarise <-  summarise(group_month, count=n(), arr_mean=mean(ArrDelay, na.rm = TRUE), arr_sd = sd(ArrDelay, na.rm = TRUE), arr_sem = sd(ArrDelay, na.rm = TRUE)/sqrt(count))
+  
+  })
+  
+  output$month_stats = DT::renderDataTable({ 
+    by_month()}, options = list(searching=FALSE, paging = FALSE), colnames = c("Month", "Count", "Mean Delay", "SD", "SEM"))
+  
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
